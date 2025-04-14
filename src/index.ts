1f880eb062dabf353aa3d09a1ae9b9517a000149
Yasuo Higa <higayasuo@gmail.com>
2025-04-15 07:00:43 +0900

    chore: add export for isWeb module in index.ts to enhance module accessibility

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './AbstractCryptoModule';
 export * from './CryptoModule';
 export * from './getCryptoModule';
+export * from './isWeb';
 export * from './NativeCryptoModule';
 export * from './WebCryptoModule';
